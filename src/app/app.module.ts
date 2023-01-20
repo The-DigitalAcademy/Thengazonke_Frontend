bd4f4acd50f8b547b72e71658ec583298f7c83e2
ntokozo-1998 <94315784+ntokozo-1998@users.noreply.github.com>
2023-01-20 16:28:17 +0200

    still pushing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RateComponent } from './components/rate/rate.component';
+import { OrderHistoryComponent } from './components/order-history/order-history.component';
+import { OrderRequestComponent } from './components/order-request/order-request.component';
+import { AccountComponent } from './components/account/account.component';
+import { NotificationsComponent } from './notifications/notifications.component';
+import { SellerComponent } from './components/seller/seller.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RateComponent,
+    OrderHistoryComponent,
+    OrderRequestComponent,
+    AccountComponent,
+    NotificationsComponent,
+    SellerComponent,
   ],
   imports: [
     BrowserModule,
