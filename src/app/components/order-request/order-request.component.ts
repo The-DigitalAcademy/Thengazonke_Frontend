7429313d34b9490d82ee002b9c3f4f3d4ce3ea70
ntokozo-1998 <94315784+ntokozo-1998@users.noreply.github.com>
2023-02-07 15:52:11 +0200

    almost done

--- src/app/components/order-request/order-request.component.ts
+++ src/app/components/order-request/order-request.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/services/auth.service';
+import { LivestockService } from 'src/app/services/livestock.service';
 import { TransactionService } from 'src/app/services/transaction.service';
 import { UserLayoutComponent } from 'src/app/user-layout/user-layout.component';
 import { UserLayoutModule } from 'src/app/user-layout/user-layout.module';
@@ -14,17 +15,21 @@ export class OrderRequestComponent implements OnInit {
   transaction!:any;
   tr!:any;
   live!:any;
-  // transaction!:any;
-  // users: Users[] = [];
   result!:any;
-  // result2!:any;
+  trans!:any;
+  result2!:any;
+  transact!:any;
+  toggleModalButton :any;
+  deleteID!:any;
+ 
 
-  constructor(private transactionService: TransactionService, private authservice: AuthService) { }
+  constructor(private transactionService: TransactionService, private authservice: AuthService, private livestockService: LivestockService) { 
+    this.getUser();
+  }
 
   users:any[] = [];
 
   ngOnInit(): void {
-
 
   }
 
@@ -42,16 +47,55 @@ export class OrderRequestComponent implements OnInit {
   getTransaction()
   {
     this.transactionService.getFullTransaction().subscribe((res:any) => {
-      this.transaction = res;
-      
-      console.log(this.transaction)
-      console.log(this.users[0].Userid)
 
-      let transTemp = this.transaction.filter((res:any) => Number(res.userID) === Number(this.users[0].Userid));
+      this.transaction = res;
+      // full trans
+      console.log(this.transaction)
+    // trans id
+      console.log(this.users[0].Userid)
+  
+      let transTemp = this.transaction.filter((res:any) => Number(res.Userid) === Number(this.users[0].Userid));
       this.tr = transTemp.filter((ress:any) => String(ress.status) != String('archieved'));
-      console.log(this.tr);
+     console.log(this.tr);
 
     }); 
+  }
+
+//   checkSelected(event:any, transID:any)
+// {
+//   // console.log(event, transID);
+
+//   this.livestockService.GetAllPostedLivestock().subscribe(async(res:any) => {
+//     this.result = await res;
+//     this.live = this.result.filter((res:any) => Number(res.livestockID) === Number(event));
+//   }); 
+
+//   this.transactionService.GetAllTransaction().subscribe(async(res:any) => {
+//     this.result2 = await res;
+//     this.transaction = this.result2.filter((res:any) => Number(res.transactionID) === Number(transID));
+//     // console.log(this.transaction);
+//   }); 
+
+// }
+  // getStatus(id:any){
+  //   this.toggleModalButton = this.result2[id].status;
+
+  //   console.log(id)
+  //   // this.getSpecificTransaction(id)
+  //   return console.log(this.toggleModalButton)
+  // }
+
+  // getSpecificTransaction(id:any){
+  //   this.transactionService.GetAllTransaction().subscribe(async(res:any) => {
+  //     this.result2 = await res;
+  //     this.transaction = this.result2.filter((res:any) => Number(res.transactionID) === Number(id));
+  //     // console.log(this.transaction);
+  //   });
+  // }
+
+  closeModal() {
+    let modalCheckbox:any = document.getElementById('my-modal')
+    modalCheckbox.checked = false
   }
 }
 
