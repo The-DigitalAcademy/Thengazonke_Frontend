7932f1b150c28355f713b4a599caaa6729da84ab
Precious-Mokone <115017921+Precious-Mokone@users.noreply.github.com>
2023-03-07 16:36:09 +0200

    bcdhsb

--- src/app/shared/cart/cart.component.ts
+++ src/app/shared/cart/cart.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/auth-layout/services/auth.service';
+import { CurrentRouteService } from '../services/current-route.service';
+import { LivestockService } from '../services/livestock.service';
+import { TransactionService } from '../services/transaction.service';
 
 @Component({
   selector: 'app-cart',
@@ -6,10 +11,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cart.component.scss']
 })
 export class CartComponent implements OnInit {
+   livestock:string[] = [];
+   livestockAll: any;
 
-  constructor() { }
+  constructor(private livestockService:LivestockService, private transactionService: TransactionService, private router: Router, private authService: AuthService) { }
 
   ngOnInit(): void {
+    this.livestockService.GetAllPostedLivestock()
+    .subscribe((res:any) => {
+      let result = res;   
+      this.livestockAll = result.filter((res:any) => String(res.status) != String("archived"));
+    });
+  }
+
+  addToCart(){
+    this.livestock.push(JSON.stringify(Date.now()));
+    localStorage.setItem("ad", JSON.stringify(this.livestock));
+
+    const storageVal = localStorage.getItem('ad');
+    console.log(storageVal)
+
+    const val = storageVal ? JSON.parse(storageVal) : []
+    val.push(JSON.stringify(Date.now()))
+
+    localStorage.setItem("ad", JSON.stringify(val));
   }
 
 }
